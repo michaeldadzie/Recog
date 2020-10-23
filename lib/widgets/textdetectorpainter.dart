a0262c8e73908a425a98253688d8bc014b04a25c
Michael Dadzie <dadziemikke2@gmail.com>
2020-10-23 17:08:30 +0000

    TextDecorator Moved to Widgets

---
+++ lib/widgets/textdetectorpainter.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+
+class TextDetectorPainter extends CustomPainter {
+  TextDetectorPainter(this.absoluteImageSize, this.elements);
+  final Size absoluteImageSize;
+  final List<TextElement> elements;
+  @override
+  void paint(Canvas canvas, Size size) {
+    final double scaleX = size.width / absoluteImageSize.width;
+    final double scaleY = size.height / absoluteImageSize.height;
+
+    Rect scaleRect(TextContainer container) {
+      return Rect.fromLTRB(
+        container.boundingBox.left * scaleX,
+        container.boundingBox.top * scaleY,
+        container.boundingBox.right * scaleX,
+        container.boundingBox.bottom * scaleY,
+      );
+    }
+
+    final Paint paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..color = Colors.red
+      ..strokeWidth = 1.5;
+
+    for (TextElement element in elements) {
+      canvas.drawRect(scaleRect(element), paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(TextDetectorPainter oldDelegate) {
+    return true;
+  }
+}
