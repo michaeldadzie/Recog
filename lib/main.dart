d98575a60336a4a44046efbeac976c978ceac5e2
Michael Dadzie <dadziemikke2@gmail.com>
2020-10-23 17:07:40 +0000

    Improve UI Code

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:camera/camera.dart';
+import 'package:recog/helpers/custom_route.dart';
+import 'screens/camera_screen.dart';
+
+List<CameraDescription> cameras = [];
+
+Future<void> main() async {
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
+    cameras = await availableCameras();
+  } on CameraException catch (e) {
+    print(e);
+  }
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent, // status bar color
+    ),
+  );
+  runApp(Recog());
+}
+
+class Recog extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Recog',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+        pageTransitionsTheme: PageTransitionsTheme(
+          builders: {
+            TargetPlatform.android: CustomPageTransitionBuilder(),
+            TargetPlatform.iOS: CustomPageTransitionBuilder(),
+          }
+        )
+      ),
+      home: CameraScreen(),
+    );
+  }
+}
