b5e5e581ca0d70102e4ce25a8551c4c96296b741
dadzi <dadziemikke2@gmail.com>
2021-01-04 15:36:53 +0000

    Improved UI code

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,8 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MainDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Drawer();
+    return Drawer(
+      child: DrawerHeader(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Michael Dadzie',
+              style: GoogleFonts.raleway(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+            SizedBox(height: 5),
+            Text(
+              'Mobile Engineer',
+              style: GoogleFonts.raleway(
+                color: Colors.black,
+                fontWeight: FontWeight.w400,
+                fontSize: 18,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
