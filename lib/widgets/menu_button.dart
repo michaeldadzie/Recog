9e4472c974b19095e00263cd49df2c4acb77b218
dadzi <dadziemikke2@gmail.com>
2021-01-10 15:37:17 +0000

    Improved UI code

--- lib/widgets/menu_button.dart
+++ lib/widgets/menu_button.dart
@@ -5,14 +5,7 @@ Widget get menuButton {
     child: Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(width: 5),
-            Container(height: 3, width: 12, color: Colors.white),
-          ],
-        ),
+      children: [
         SizedBox(height: 7),
         Container(height: 3, width: 24, color: Colors.white),
         SizedBox(height: 7),
